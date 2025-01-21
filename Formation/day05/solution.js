76608e482344fb08a10dff6f9e5b45cdc384f300
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-01-20 23:22:20 -0800

    update solution for Formation day 5 challenge

--- Formation/day05/solution.js
+++ Formation/day05/solution.js
@@ -1,5 +1,5 @@
 monotonicMatrix = [[0, 0, 0, 1], [1, 1, 1, 2], [2, 3, 4, 5]]
-nonMonotonicMatrix = [[0, 0, 0, 1], [1, 1, 3, 2], [2, 3, 4, 5]] 
+nonMonotonicMatrix = [[0, 0, 0, 1], [1, 1, 1, 0], [2, 3, 4, 5]] 
 console.log(`isMatrixMonotonic(monotonicMatrix): ${isMatrixMonotonic(monotonicMatrix)}`);
 console.log(`isMatrixMonotonic(nonMonotonicMatrix): ${isMatrixMonotonic(nonMonotonicMatrix)}`);
 
@@ -8,6 +8,9 @@ function isMatrixMonotonic(matrix) {
 		let min = matrix[i][0];
 		for (let j = 0; j < matrix[0].length; j++) {
 			if (matrix[i][j] < min) {
+				return false;
+			}
+			if (i > 0 && matrix[i][j] < matrix[i-1][j]) {
 				return false;
 			}
 			min = matrix[i][j];
