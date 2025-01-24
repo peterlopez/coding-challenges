791d32ecc37d37a1cfee85c6fb4fbd5734c60ece
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-01-23 23:50:43 -0800

    update solution for Formation day 7 challenge
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- Formation/day07/solution.js
+++ Formation/day07/solution.js
@@ -1,43 +1,81 @@
-console.assert(isPalindromeAnagram("mmo") === true, "'mmo' is a palindrome anagram");
-console.assert(isPalindromeAnagram("abc") === false, "'abc' is not a palindrome anagram");
-console.assert(isPalindromeAnagram("cerarac") === true, "'cerarac' is a palindrome anagram");
+console.assert(
+  isPalindromeAnagram("mmo") === true,
+  "'mmo' is a palindrome anagram"
+);
+console.assert(
+  isPalindromeAnagram("abc") === false,
+  "'abc' is not a palindrome anagram"
+);
+console.assert(
+  isPalindromeAnagram("cerarac") === true,
+  "'cerarac' is a palindrome anagram"
+);
 
 function isPalindromeAnagram(word) {
+  const answerUsingLetterCount = solveUsingLetterCount(word);
 
-	function getPermutations(str) {
-		if (str.length === 1) {
-			return [str];
-		}
+  function solveUsingLetterCount(word) {
+    const letters = new Map();
 
-		const permutations = [];
-		for (let i = 0; i < str.length; i++) {
-			const remainingPermutations = getPermutations(str.slice(0, i) + str.slice(i + 1));
-			permutations.push(...remainingPermutations.map((permutation) => str[i] + permutation ));
-		}
-		return permutations;
-	}
+    for (let letter of word) {
+      const count = letters.get(letter) ?? 0;
+      letters.set(letter, count + 1);
+    }
 
-	function isPalindrome(str) {
-		let start = 0;
-		let end = str.length - 1;
+    let numOddLetters = 0;
+    for (const [letter, count] of letters) {
+      if (count % 2 !== 0) {
+        numOddLetters++;
+      }
+    }
 
-		while (start < end) {
-			if (str[start] != str[end]) {
-				return false;
-			}
-			start++;
-			end--;
-		}
+    return numOddLetters === 0 || numOddLetters === 1;
+  }
 
-		return true;
-	}
+  const answerUsingPermutations = solveUsingPermutations(word);
 
-	const permutations = getPermutations(word);
+  function solveUsingPermutations(word) {
+    function getPermutations(str) {
+      if (str.length === 1) {
+        return [str];
+      }
 
-	for (const perm of permutations) {
-		if (isPalindrome(perm)) return true;
-	}
+      const permutations = [];
+      for (let i = 0; i < str.length; i++) {
+        const remainingPermutations = getPermutations(
+          str.slice(0, i) + str.slice(i + 1)
+        );
+        permutations.push(
+          ...remainingPermutations.map((permutation) => str[i] + permutation)
+        );
+      }
+      return permutations;
+    }
 
-	return false;
+    function isPalindrome(str) {
+      let start = 0;
+      let end = str.length - 1;
+
+      while (start < end) {
+        if (str[start] != str[end]) {
+          return false;
+        }
+        start++;
+        end--;
+      }
+
+      return true;
+    }
+
+    const permutations = getPermutations(word);
+
+    for (const perm of permutations) {
+      if (isPalindrome(perm)) return true;
+    }
+
+    return false;
+  }
+
+  console.assert(answerUsingLetterCount === answerUsingPermutations);
+  return answerUsingLetterCount;
 }
-
