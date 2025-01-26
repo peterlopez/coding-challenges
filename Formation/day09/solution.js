d9c50e10e7032e94464edc8f024e689bf1a4be2c
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-01-25 22:44:48 -0800

    update solution for Formation day 9 challenge
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- Formation/day09/solution.js
+++ Formation/day09/solution.js
@@ -1,17 +1,17 @@
 function binarySearch(data, k) {
-  let left = 0;
+  let left = -1;
   let right = data.length;
 
-  while (left <= right) {
+  while (right - left > 1) {
     let mid = left + Math.floor((right - left) / 2);
     let val = data[mid];
 
     if (val === k) {
       return mid;
     } else if (val < k) {
-      left = mid + 1;
+      left = mid;
     } else {
-      right = mid - 1;
+      right = mid;
     }
   }
 
@@ -23,4 +23,19 @@ console.assert(
   binarySearch(sortedArray, 1) === 0 &&
     binarySearch(sortedArray, 2) === -1 &&
     binarySearch(sortedArray, 13) === 6
+);
+
+console.assert(
+  binarySearch(sortedArray, 21) === 10 &&
+    binarySearch(sortedArray, 20) === -1 &&
+    binarySearch(sortedArray, 11) === 5
+);
+
+const emptyArray = [];
+console.assert(binarySearch(emptyArray, 1) === -1);
+
+const singleElementArray = [10];
+console.assert(
+  binarySearch(singleElementArray, 10) === 0 &&
+    binarySearch(singleElementArray, 5) === -1
 );
